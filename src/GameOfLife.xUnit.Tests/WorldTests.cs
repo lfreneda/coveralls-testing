e4209b84fc6c8bafcff92fdd6560cb6388c135a6
Luiz Freneda <lfreneda@gmail.com>
2015-06-12 17:12:50 -0300

    meooooooooooo deoz

--- src/GameOfLife.xUnit.Tests/WorldTests.cs
+++ src/GameOfLife.xUnit.Tests/WorldTests.cs
@@ -4,6 +4,7 @@ namespace GameOfLife.xUnit.Tests
 {
     public class WorldTests
     {
+        /*
         [Fact]
         public void World_ConstructNew_WidthCorrect()
         {
@@ -14,16 +15,17 @@ namespace GameOfLife.xUnit.Tests
 
             Assert.Equal(expectedWidth, world.Width);
         }
+         */
 
-        //[Fact]
-        //public void World_ConstructNew_HeightCorrect()
-        //{
-        //    const int expectedHeight = 5;
-        //    var data = new bool[12, expectedHeight];
+        [Fact]
+        public void World_ConstructNew_HeightCorrect()
+        {
+            const int expectedHeight = 5;
+            var data = new bool[12, expectedHeight];
 
-        //    var world = new World(data);
+            var world = new World(data);
 
-        //    Assert.Equal(expectedHeight, world.Height);
-        //}
+            Assert.Equal(expectedHeight, world.Height);
+        }
     }
 }
